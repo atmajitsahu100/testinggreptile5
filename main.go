1c260aea93dfb205fd2963b5aa2a539eae292c39
atmajitsahu100 <atmajitsahoo@gmail.com>
2025-01-08 23:08:37 +0530

    first master commit

---
+++ main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type SecretHandler struct {
+	SecretKey string
+}
+
+func NewSecretHandler(key string) *SecretHandler {
+	return &SecretHandler{SecretKey: key}
+}
+
+func (h *SecretHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path == "/get-secret" {
+		h.handleSecretRequest(w, r)
+		return
+	}
+	http.NotFound(w, r)
+}
+
+func (h *SecretHandler) handleSecretRequest(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"secret": h.SecretKey}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
+}
+
+func main() {
+	secretKey := "adnsjfsdfffdsssfsfdsfdfdfsadadada12345"
+
+	mux := http.NewServeMux()
+	secretHandler := NewSecretHandler(secretKey)
+	mux.Handle("/get-secret", secretHandler)
+
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: mux,
+	}
+
+	fmt.Println("Starting server on :8080")
+	err := server.ListenAndServe()
+	if err != nil {
+		log.Fatalf("Server failed: %s", err)
+	}
+}
