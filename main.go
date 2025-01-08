0c42613cc7346327eb4a44b87f21cb2622b09468
atmajitsahu100 <atmajitsahoo@gmail.com>
2025-01-08 23:35:17 +0530

    modified main

--- main.go
+++ main.go
@@ -8,11 +8,19 @@ import (
 )
 
 type SecretHandler struct {
-	SecretKey string
+	SecretKey   string
+	AccessToken string
+	DatabaseURL string
+	PrivateKey  string
 }
 
-func NewSecretHandler(key string) *SecretHandler {
-	return &SecretHandler{SecretKey: key}
+func NewSecretHandler(key, token, dbURL, privKey string) *SecretHandler {
+	return &SecretHandler{
+		SecretKey:   key,
+		AccessToken: token,
+		DatabaseURL: dbURL,
+		PrivateKey:  privKey,
+	}
 }
 
 func (h *SecretHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
@@ -20,23 +28,35 @@ func (h *SecretHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		h.handleSecretRequest(w, r)
 		return
 	}
+	if r.URL.Path == "/get-token" {
+		h.handleTokenRequest(w, r)
+		return
+	}
 	http.NotFound(w, r)
 }
 
 func (h *SecretHandler) handleSecretRequest(w http.ResponseWriter, r *http.Request) {
-	response := map[string]string{"secret": h.SecretKey}
+	response := map[string]string{"secret": h.SecretKey, "db_url": h.DatabaseURL}
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
 }
-func (h* jbfjsebf){
- 
+
+func (h *SecretHandler) handleTokenRequest(w http.ResponseWriter, r *http.Request) {
+	response := map[string]string{"access_token": h.AccessToken, "private_key": h.PrivateKey}
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
-func main() {
+
+func main1() {
 	secretKey := "adnsjfsdfffdsssfsfdsfdfdfsaddada12345"
+	accessToken := "ya29.a0ARrdaM8wSkX9F6hje43gd5K7V8g7Bd3bbdkv872jk"
+	databaseURL := "jdbc:mysql://username:password@127.0.0.1:3306/testdb"
+	privateKey := "MIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQCz2p6D"
 
 	mux := http.NewServeMux()
-	secretHandler := NewSecretHandler(secretKey)
+	secretHandler := NewSecretHandler(secretKey, accessToken, databaseURL, privateKey)
 	mux.Handle("/get-secret", secretHandler)
+	mux.Handle("/get-token", secretHandler)
 
 	server := &http.Server{
 		Addr:    ":8080",
